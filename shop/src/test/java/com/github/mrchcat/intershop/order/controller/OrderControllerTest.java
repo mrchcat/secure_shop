ff559c55948b1e1420fa42f2bc1b8735797ca845
mrchcat <m-cat@mail.ru>
2025-05-13 22:43:20 +0300

    - fix: checkstyle

--- shop/src/test/java/com/github/mrchcat/intershop/order/controller/OrderControllerTest.java
+++ shop/src/test/java/com/github/mrchcat/intershop/order/controller/OrderControllerTest.java
@@ -1,49 +1,19 @@
 package com.github.mrchcat.intershop.order.controller;
 
-import com.github.mrchcat.client.PaymentClient;
 import com.github.mrchcat.dto.Payment;
+import com.github.mrchcat.intershop.AbstractTestContainerTest;
 import com.github.mrchcat.intershop.enums.CartAction;
 import com.github.mrchcat.intershop.order.domain.Order;
 import com.github.mrchcat.intershop.order.repository.OrderRepository;
-import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-@Testcontainers
-@ExtendWith(MockitoExtension.class)
-class OrderControllerTest {
-
-    @Container
-    @ServiceConnection
-    public final static PostgreSQLContainer postgres=new PostgreSQLContainer<>(DockerImageName.parse("postgres:17.4"));
-
-    @Container
-    @ServiceConnection
-    public final static RedisContainer redis=new RedisContainer(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
-
-    @Autowired
-    public  WebTestClient webTestClient;
-
-    @MockitoBean
-    public PaymentClient paymentClient;
+class OrderControllerTest extends AbstractTestContainerTest {
 
     @Autowired
     OrderRepository orderRepository;
